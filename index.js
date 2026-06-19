141cf253e0918b819e4d89d50940d233ca586d04
FatmaMoataz <fatmamoataz65@gmail.com>
2026-06-19 18:20:13 +0200

    subtask.model

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ import './models/meeting.model.js';
 import './models/meetingLog.model.js';
 import './models/refreshToken.model.js';
 import './models/board.model.js';
-import './models/Subtask.model.js';
+import './models/subtask.model.js';
 
 // Middlewares
 app.use(express.json());
